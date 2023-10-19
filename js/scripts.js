1b7d4b8d57e0f526483126d6d2ec4794607d8c8b
Brianca Knight <briancaknight@gmail.com>
2023-10-19 15:14:38 -0700

    Refactored code
    Co-authored-by: Taj Empey <tajempey@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,37 @@
+// Buiness Logic
 function calculate(event) {
   event.preventDefault();
   const number1 = parseInt(document.querySelector("input#input1").value);
   const number2 = parseInt(document.querySelector("input#input2").value);
   const number3 = parseInt(document.querySelector("input#input3").value);
-  console.log(number1)
-  console.log(number2)
-  console.log(number3)
- 
+  const num1_2 = number1 + number2;
+  const num2_3 = number2 + number3;
+  const num3_1 = number3 + number1;
+
+  // User Interface Logic
   let answer;
   if (number1 && number2 && number3) {
-    if (number1 === number2 && number2 === number3) {
+    if (number3 >= num1_2 || number1 >= num2_3 || number2 >= num3_1) {
+      answer = "Not a triangle";
+      document.getElementById("output").innerText = answer;
+      document.getElementById("output-display").removeAttribute("class");
+    } else if (number1 === number2 && number2 === number3) {
       answer = "Equilateral";
+      document.getElementById("output").innerText = answer;
+      document.getElementById("output-display").removeAttribute("class");
     } else if (number1 !== number2 && number2 !== number3) {
       answer = "Scalene"
-    } else if (number1 === number2 && number2 !== number3) {
+      document.getElementById("output").innerText = answer;
+      document.getElementById("output-display").removeAttribute("class");
+    } else if ((number1 === number2 && number2 !== number3) || (number1 === number3 && number3 !== number2) || (number3 === number2 && number2 !== number1)) {
       answer = "Isosceles";
-    } else if (number1 === number3 && number3 !== number2) {
-      answer = "Isosceles";
-    } else if (number3 === number2 && number2 !== number1) {
-      answer = "Isosceles";
-      document.getElementById("answer").innerText = answer;
+      document.getElementById("output").innerText = answer;
+      document.getElementById("output-display").removeAttribute("class");
     }
-    document.getElementById("answer").innerText = answer;
+  } else {
+    answer = "Please fill out the form correctly";
+    document.getElementById("output").innerText = answer;
+    document.getElementById("output-display").removeAttribute("class");
   }
 }
 
